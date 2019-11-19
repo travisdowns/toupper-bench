e54375b26e1396374a06fd56e2ef702439a9610d
tdowns <travis.downs@gmail.com>
2019-11-19 17:42:53 -0500

    swap stlib and ctype so it works on more glibc versions

--- impls-noalgo.cpp
+++ impls-noalgo.cpp
@@ -2,8 +2,8 @@
  * Implementation of various very basic algorithms, mostly as a litmus test for the more complicated ones.
  */
 
-#include <stdlib.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 void toupper_rawloop(char* buf, size_t size) {
     for (size_t i = 0; i < size; i++) {
